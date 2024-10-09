2be92bcb132d7888b3873acb969220473c162d62
elprofessor67 <jeeshanr599@gmail.com>
2024-10-09 17:31:20 +0530

    restrucre route

---
+++ shared/types/webRTCTypes.ts
@@ -0,0 +1,44 @@
+import * as mediasoup from 'mediasoup'
+import { IceCandidate } from 'mediasoup/node/lib/fbs/web-rtc-transport'
+
+export interface IRoomArguments {
+    room_id: string;
+    username: string;
+    isMicMute: boolean;
+    isWebCamMute: boolean
+}
+
+export interface ITransportFunctionArguments {
+    consumer: boolean
+}
+
+export interface IPramas {
+
+    id?: string;
+    iceParameters?: mediasoup.types.IceParameters;
+    iceCandidates?: mediasoup.types.IceCandidate[];
+    dtlsParameters?: mediasoup.types.DtlsParameters;
+    error?: string
+}
+
+
+export interface IProceTransportAguments
+{
+    kind:mediasoup.types.MediaKind;
+    rtpParameters:mediasoup.types.RtpParameters;
+    appData:mediasoup.types.AppData
+}
+
+export interface IProducerIds{
+    producerId: string;
+    socketId: string;
+}
+
+
+
+export interface IComsumeMediaArgument
+{
+    rtpCapabilities: mediasoup.types.RtpCapabilities;
+    remoteProducerId: string;
+    serverConsumerTransportId: string;
+}
